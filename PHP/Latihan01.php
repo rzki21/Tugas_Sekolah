03b533648b11733289bb0fa800d3fea5d26e09ca
Muhammad Rizki <118183004+rzki21@users.noreply.github.com>
2025-01-20 16:39:39 +0700

    Latihan01.php

---
+++ PHP/Latihan01.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2 >Identitas Murid XI TKJ 1</h2>
+</body>
+</html>
+
+<?php //Deklarasi PHP Di Sini!
+   $nama = "Dava Mochamad";
+   $umur = "17 Tahun";
+   $AsalSekolah = "SMK JEPANG";
+   echo "Nama: $nama <br>";
+   echo "Umur : $umur <br>";
+   echo "Asal Sekolah : $AsalSekolah <br>" 
+?>
+<?php
+$bidang = "Panjang";
+$nilai_sisi_1 = 10;
+$nilai_sisi_2 = 30;
+$hasil = $nilai_sisi_1 + $nilai_sisi_2;
+
+echo "hasil dari tambahan $bidang $nilai_sisi_1 + $nilai_sisi_2 = " .$hasil;
+
+?>
